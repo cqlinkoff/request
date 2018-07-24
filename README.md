bf7796a2d2d65f9c1a0490315f1ea1169cf98baf
kagawagao <kingsongao1221@gmail.com>
2018-07-24 16:24:24 +0800

    feat: add request

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # request
-http request based on fetch api
+
+> http request based on [`fetch`](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API) api
+
+[![node](https://img.shields.io/node/v/@cqmbr/request.svg)](https://www.npmjs.com/package/@cqmbr/request)
+[![npm](https://img.shields.io/npm/v/@cqmbr/request.svg)](https://www.npmjs.com/package/@cqmbr/request)
+[![license](https://img.shields.io/npm/l/@cqmbr/request.svg)](https://github.com/cqmbr/request/blob/master/LICENSE)
+[![Build Status](https://travis-ci.org/cqmbr/request.svg?branch=master)](https://travis-ci.org/cqmbr/request)
+[![Standard - JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](http://standardjs.com/)
+[![Coverage Status](https://coveralls.io/repos/github/cqmbr/request/badge.svg?branch=master)](https://coveralls.io/github/cqmbr/request?branch=master)
+
+## Installation
+
+```bash
+npm i @cqmbr/request
+```
+
+## Use
+
+```js
+import Request from '@cqmbr/request'
+
+const request = new Request(options)
+```
+
+## API
+
+### `new Request(options)`
+
+> create a request instance
+
+- `options`:
+  - `options.baseURL`: base url
+  - `options.headers`: default headers
+  - `options.beforeRequest`: will be called before request, you can handle with `request` config and return a new config, support `Promise`
+  - `options.afterRequest`: will be called after request, you can handle with `response` and return it, support `Promise`
+
+### `request.request(url, requestOptions)`
+
+> basic http request
+
+- `url`: `string` or `object`, if it's `object`, it will be treated as `requestOptions`, see [`requestOptions`](#requestoptions)
+- `requestOptions`: see [`requestOptions`](#requestoptions)
+
+### `request.get(url, requestOptions): Promise<any>`
+
+> request use `GET`
+
+### `request.post(url, requestOptions): Promise<any>`
+
+> request use `POST`
+
+### `request.put(url, requestOptions): Promise<any>`
+
+> request use `PUT`
+
+### `request.patch(url, requestOptions): Promise<any>`
+
+> request use `PATCH`
+
+### `request.del(url, requestOptions): Promise<any>`
+
+> request use `DELETE`
+
+## `requestOptions`
+
+> request config, may have follow keys:
+
+- `url`: `string`
+> resource url, it will be automatically added after `baseURL` if you set it in constructor, support `params`
+- `query`: `object`
+> query params, it will be automatically added after `url`
+- `body`: `object`
+> request body, if request method is `GET`, it's same as `query`
+- `params`: `object`
+> url params will be automatically replaced based on `params`, such as `/{id}` will be replaced by `params.id`
+- `headers`: `object`
+> request headers
+
+## TODO
+
+- [ ] unit test
---
+++ src/index.js
@@ -0,0 +1,151 @@
+import qs from 'query-string'
+import template from 'string-template'
+import isPlainObject from 'lodash/isPlainObject'
+import isString from 'lodash/isString'
+
+const noop = (arg) => arg
+
+export default class Request {
+  constructor (options = {}) {
+    const {
+      baseURL,
+      headers,
+      beforeRequest = noop,
+      afterRequest = noop
+    } = options
+
+    this.baseURL = baseURL
+    this.defaultHeaders = headers
+    this.beforeRequest = beforeRequest
+    this.afterRequest = afterRequest
+  }
+
+  __getBody = async (res) => {
+    const type = res.headers.get('Content-Type')
+
+    if (type && type.indexOf('json') !== -1) {
+      const json = await res.json()
+      return json
+    }
+
+    const body = await res.text()
+
+    try {
+      return JSON.parse(body)
+    } catch (error) {
+      return { body }
+    }
+  }
+
+  __parseReq = ({ url, query, params, body, baseURL = this.baseURL, ...req }) => {
+    if (body) {
+      if (typeof body === 'object') {
+        if (/^(POST|PUT|PATCH)$/i.test(req.method)) {
+          body = JSON.stringify(body)
+        } else {
+          url += ((url.indexOf('?') !== -1) ? '&' : '?') + qs.stringify(body)
+          body = null
+        }
+      }
+      if (body) {
+        req.body = body
+      }
+    }
+
+    if (query) {
+      if (typeof query === 'object') {
+        query = qs.stringify(query)
+      }
+
+      if (query) {
+        url += (url.indexOf('?') === -1 ? '?' : '&') + query
+      }
+    }
+
+    if (params) {
+      url = template(url, params)
+    }
+    if (/^\/[^/]/.test(url) && baseURL) {
+      url = baseURL + url
+    }
+
+    req.url = url
+    return req
+  }
+
+  request = async (url, options = {}) => {
+    if (isPlainObject(url)) {
+      options = { ...url }
+    }
+
+    if (!isPlainObject(options)) {
+      throw new TypeError('Options must be an object!')
+    }
+
+    if (isString(url)) {
+      options.url = url
+    }
+
+    if (isPlainObject(options.headers)) {
+      options.headers = { ...this.defaultHeaders, ...options.headers }
+    } else {
+      options.headers = this.defaultHeaders
+    }
+
+    if (!isString(options.url)) {
+      throw new TypeError('URL must be string')
+    }
+
+    let req = this.__parseReq(options)
+
+    try {
+      if (typeof this.beforeRequest === 'function') {
+        req = await this.beforeRequest(req)
+      }
+
+      let res = await window.fetch(req.url, req)
+      if (typeof this.afterRequest === 'function') {
+        res = await this.afterRequest(res)
+      }
+      const body = await this.__getBody(res)
+
+      if (res.status >= 200 && res.status < 400) {
+        return body
+      } else {
+        throw body
+      }
+    } catch (error) {
+      throw error
+    }
+  }
+
+  get = (url, req = {}) => {
+    req.url = url
+    req.method = 'GET'
+    return this.request(req)
+  }
+
+  post = (url, req = {}) => {
+    req.url = url
+    req.method = 'POST'
+    return this.request(req)
+  }
+
+  put = (url, req = {}) => {
+    req.url = url
+    req.method = 'PUT'
+    return this.request(req)
+  }
+
+  patch = (url, req = {}) => {
+    req.url = url
+    req.method = 'PATCH'
+    return this.request(req)
+  }
+
+  del = (url, req = {}) => {
+    req.url = url
+    req.method = 'DELETE'
+    return this.request(req)
+  }
+}
